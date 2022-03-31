717c3b2b3d55d297b667977ab1a416add4eb22a4
KristinaGalche <hit-tb@mail.ru>
2022-03-31 23:12:12 +1000

    3 tests passed

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,14 +2,6 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-//    public static void main(String[] args) {
-//        int lowerLimit = 200;
-//        int upperLimit = 500;
-//
-//        SQRService service = new SQRService();
-//        System.out.println("Количество квадратов: " + service.calculateNumberSqr(lowerLimit, upperLimit, count));
-//    }
-
     public int calculateNumberSqr(int lowerLimit, int upperLimit) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
